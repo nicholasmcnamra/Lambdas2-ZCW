6cbfc1d2c1a119af5368e9b165c0a65444bb01cd
nicholasmcnamra <nikomcnamara@gmail.com>
2024-04-08 14:34:01 -0400

    finished

--- src/test/java/rocks/zipcode/lambdas/LambdaTests.java
+++ src/test/java/rocks/zipcode/lambdas/LambdaTests.java
@@ -1,5 +1,6 @@
 package rocks.zipcode.lambdas;
 import org.testng.annotations.Test;
+
 import java.time.LocalDate;
 import java.util.Objects;
 import java.util.regex.Pattern;
@@ -8,7 +9,7 @@ import static rocks.zipcode.lambdas.Person.Gender.FEMALE;
 import static rocks.zipcode.lambdas.Person.Gender.NONBINARY;
 import static rocks.zipcode.lambdas.Person.printPersons;
 
-public class AnonymousClassTests {
+public class LambdaTests {
 
     @Test
     public void validateAgeTest() {
@@ -16,7 +17,7 @@ public class AnonymousClassTests {
         LocalDate birthDate = LocalDate.of(1995,2,25);
         Person.Gender gender = NONBINARY;
         String email = "jesse@gmail.com";
-        Person person = new Person(name, birthDate, gender, email);
+        Person person1 = new Person(name, birthDate, gender, email);
 
         String name2 = "Juana";
         LocalDate birthDate2 = LocalDate.of(2000,3,7);
@@ -24,12 +25,7 @@ public class AnonymousClassTests {
         String email2 = "juana.gmail.com";
         Person person2 = new Person(name2, birthDate2, gender2, email2);
 
-        printPersons(Person.getRoster(), new CheckPerson() {
-            @Override
-            public boolean test(Person person) {
-                return person.getAge() >= 18;
-            }
-        });
+        printPersons(Person.getRoster(), (Person p) -> p.getAge() >= 18);
     }
 
     @Test
@@ -46,12 +42,7 @@ public class AnonymousClassTests {
         String email2 = "juana.gmail.com";
         Person person2 = new Person(name2, birthDate2, gender2, email2);
 
-        printPersons(Person.getRoster(), new CheckPerson() {
-            @Override
-            public boolean test(Person person) {
-                return Objects.equals(person.getGender(), "Female");
-            }
-        });
+        printPersons(Person.getRoster(), (Person p)-> p.getGender().equals("Non-binary"));
     }
 
     @Test
@@ -69,14 +60,7 @@ public class AnonymousClassTests {
         Person person2 = new Person(name2, birthDate2, gender2, email2);
 
         Pattern emailPattern = Pattern.compile("^(.+)@(.+)$");
-        printPersons(Person.getRoster(), new CheckPerson() {
-            @Override
-            public boolean test(Person person) {
-                if (emailPattern.matcher(person.getEmailAddress()).matches())
-                    return true;
-                return false;
-            }
-        });
+        printPersons(Person.getRoster(), (Person p) -> emailPattern.matcher(p.getEmailAddress()).matches());
     }
 
     @Test
@@ -94,15 +78,6 @@ public class AnonymousClassTests {
         Person person2 = new Person(name2, birthDate2, gender2, email2);
 
         Pattern emailPattern = Pattern.compile("^(.+)@(.+)?");
-        printPersons(Person.getRoster(), new CheckPerson() {
-            @Override
-            public boolean test(Person person) {
-                if (!emailPattern.matcher(person.getEmailAddress()).matches())
-                    return true;
-                return false;
-            }
-        });
+        printPersons(Person.getRoster(), (Person p) -> !emailPattern.matcher(p.getEmailAddress()).matches());
     }
-
-
 }
